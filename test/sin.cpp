9456971b23a53ad3d015657fb1b25ec3605a63b2
rishit-singh <rsrishitsingh@gmail.com>
2021-07-10 21:49:43 +0530

    Fixed Sin() output.
    Changes:
    	1. Changed the argument type of Sin() from int to double.
    
    Bugs:
    	Null!

--- test/sin.cpp
+++ test/sin.cpp
@@ -9,19 +9,19 @@ double GetFactorial(int n)
 	return (n == 0) ? 1 : (n * GetFactorial(n - 1));
 }
 
-double GetRadians(int angle)
+double GetRadians(double angle)
 {
 	return (angle * (PI / 180));
 }
 
-double Sin(int angle)
+double Sin(double angle)
 {	
 	double sinResult = 0;
 
 	angle = GetRadians(angle);
 	
 	for (int x = 0; x < PRECISION; x++)
-		sinResult += ((pow(-1, x) * pow(angle, (2 * x) + 1)) / GetFactorial((2 * x) + 1));
+		sinResult += ((pow(-1, x) * pow(angle, (2 * x) + 1) / GetFactorial((2 * x) + 1)));
 
 	return sinResult;
 }
--- test/sin.h
+++ test/sin.h
@@ -4,8 +4,8 @@
 #define PRECISION 10
 
 double GetFactorial(int);	//	Returns the factorial of the provided number.
-double GetRadians(int);	//	Converts provide angle in degrees to radians. 
+double GetRadians(double);	//	Converts provide angle in degrees to radians. 
 
-double Sin(int);    //  Returns the sine of the provided angle in degrees, based on the Taylor series.
+double Sin(double);    //  Returns the sine of the provided angle in degrees, based on the Taylor series.
 
 #endif
