46247e1d7dd104b2c5097db9dd00d60c01152d45
rishit-singh <rsrishitsingh@gmail.com>
2021-07-10 21:25:04 +0530

    Added Sin function implementation.
    Bugs:
    	1. Undefined behaviour.

---
+++ test/sin.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include "sin.h"
+
+double GetFactorial(int n)
+{
+	return (n == 0) ? 1 : (n * GetFactorial(n - 1));
+}
+
+double GetRadians(int angle)
+{
+	return (angle * (PI / 180));
+}
+
+double Sin(int angle)
+{	
+	double sinResult = 0;
+
+	angle = GetRadians(angle);
+	
+	for (int x = 0; x < PRECISION; x++)
+		sinResult += ((pow(-1, x) * pow(angle, (2 * x) + 1)) / GetFactorial((2 * x) + 1));
+
+	return sinResult;
+}
+
+int main(int argc, char** argv)
+{	
+	double number = atof(argv[1]);
+
+	std::cout << "Sin " << number << ": " << Sin(number) << '\n';
+
+	return 0;
+}
+
+
---
+++ test/sin.h
@@ -0,0 +1,11 @@
+#ifndef SIN_H
+
+#define PI 3.1415926535
+#define PRECISION 10
+
+double GetFactorial(int);	//	Returns the factorial of the provided number.
+double GetRadians(int);	//	Converts provide angle in degrees to radians. 
+
+double Sin(int);    //  Returns the sine of the provided angle in degrees, based on the Taylor series.
+
+#endif
